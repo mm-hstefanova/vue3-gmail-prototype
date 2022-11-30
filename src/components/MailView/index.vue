a02cb83c7945510b19a1f165e33c7cfdd4635f2c
hstefanova <hristiyana.stefanova@mentormate.com>
2022-11-30 19:32:48 +0200

    wip

---
+++ src/components/MailView/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ item.subject }}</p>
+
+  <p>
+    <strong>{{ item.from }}</strong>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: null
+    }
+  },
+  setup(props) {
+    return {};
+  }
+};
+</script>
